ec659efaada320839d63d8f6e6cea97d6559b96b
RyanEggert <Ryan.Eggert@students.olin.edu>
2014-04-14 14:21:36 -0400

    Remove redundant variable

--- FFT_Test.m
+++ FFT_Test.m
@@ -14,7 +14,6 @@ for i = 1:300
     recordblocking(recObj, chunkLength);
     
 
-
     y = getaudiodata(recObj);
 
    figure(1)
@@ -23,10 +22,8 @@ for i = 1:300
     axis([0 samplesPerChunk -.3 .3])
     
     
-    samplesize = size(y);
-    L = samplesize(1);
-    NFFT = 2^nextpow2(L); % Next power of 2 from length of y
-    Y = fft(y,NFFT)/L;
+    NFFT = 2^nextpow2(samplesPerChunk); % Next power of 2 from length of y
+    Y = fft(y,NFFT)/samplesPerChunk;
     f = micSampFreq/2*linspace(0,1,NFFT/2+1);
     
     figure(2)
