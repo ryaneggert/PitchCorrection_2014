0cfee1f81b4727a4a045e5342fe6ed5c65f15047
RyanEggert <Ryan.Eggert@students.olin.edu>
2014-04-16 12:30:58 -0400

    Numpy FFT
    FFT of a sine function using numpy and matplotlib

---
+++ numpy_test.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+pi = np.pi
+
+t = np.arange(0,20.48, .0001)
+sp = np.fft.fft(np.sin(2*pi*440*t))
+freq = np.fft.fftfreq(t.shape[-1], .0001)
+print t.shape[-1]
+plt.plot(freq, sp.real)
+plt.show()
