26062849fa7eaa5b9afe6785fa1ce9bef104a751
greenteawarrior <emily.wang@students.olin.edu>
2014-04-19 15:20:11 -0400

    plotting the DFT of input signals

--- pyaudio-tests.py
+++ pyaudio-tests.py
@@ -1,7 +1,7 @@
 import pyaudio
 import wave
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 def recording(seconds, outputFilename):
     """
@@ -57,20 +57,23 @@ def recording(seconds, outputFilename):
     return
 
 if __name__ == "__main__" :
+    #create a wav to tinker with
     #recording(5, "oh hey")
+    
+    #load a file and get the data / necessary params
     waver = wave.open('oh hey.wav', mode='r')
-    data = waver.readframes(-1)
+    inputData = waver.readframes(-1)
+    inputData = np.fromstring(inputData, 'Int16')
+    fs = waver.getframerate() #sampling frequency
 
-    data = np.fromstring(data, 'uint8')
-    data = data / 2**15.
-    fs = waver.getframerate()
-    print fs
+    #DFT shenanigans (using numpy's fft)
+    FFTinput = np.fft.fft(inputData)
+    omega = np.linspace(-1000, 1000, num=len(FFTinput))
 
-    # print waver
-    # print waver.getnchannels()
-    # print waver.getsampwidth()
-    # print waver.getframerate()
-    # print waver.getnframes()
-    # print waver.getcomptype()
-    # readframes =  waver.readframes(10)
-    # print [ord(i) for i in readframes]
+    #plotting the thing in frequency domain
+    plt.figure(1)
+    plt.title('DFT of input')
+    plt.plot(omega, FFTinput)
+    plt.show()
+
+
